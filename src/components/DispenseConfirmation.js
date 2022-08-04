03c81d9b3c2cf9443594f570dea8682ab17d86f0
Eman Youssef <emanessam@hotmail.com>
2022-08-04 21:58:29 +1000

    confirmation for dispensing

---
+++ src/components/DispenseConfirmation.js
@@ -0,0 +1,12 @@
+import PatientDetail from "./PatientDetails"
+
+const DispenseConfirmation = () => {
+    return (
+        <>
+        <PatientDetail>
+        <h1> Drug has been successfuly dispnesed Current balance is:</h1>
+        </PatientDetail>
+        </>
+    )
+} 
+export default DispenseConfirmation  
